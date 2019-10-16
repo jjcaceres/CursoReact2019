0886a8f181234e199649bacc84832513099421ad
Jose <jose.caceres.mamani@gmail.com>
2019-10-16 16:46:40 -0500

    details

---
+++ Clase-01/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "react-dom";
+import { Student } from "./Components/Student";
+
+const root = document.getElementById("root");
+
+const Title = () => (
+  <h1 id="hello-word" className="app-title">
+    Hello WORLD
+  </h1>
+);
+
+const App = () => (
+  <div>
+    <Title />
+    <Student
+      name="Paul Diaz"
+      company="Cignium Technologies"
+      background="Software Engineer"
+    />
+    <Student name="Luis Ruiz" company="Freelance" />
+    <Student name="Andres Savedra" company="Cibertec" background="otro" />
+  </div>
+);
+
+render(<App />, root);
---
+++ Clase-01/src/Components/Student.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const Student = ({ name, company, background = "SE" }) => (
+  <div className="student">
+    <h3>{name}</h3>
+    <p>{company}</p>
+    <span>{background}</span>
+  </div>
+);
